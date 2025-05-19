5c3a3d78cc16e027fe8b386b5c53909df7ad3cfa
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-05-18 21:15:13 -0300

    Programa que implementa o meme do Fluxograma da Engenharia

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("--- Fluxograma da Engenharia ---");
+Console.WriteLine("\nAviso: Responda apenas com as letras S e N");
+Console.WriteLine("\nO objeto está se movendo? (S/N) ");
+bool movendo = Console.ReadLine()!.ToUpper().Trim().Substring(0, 1) == "S";
+Console.WriteLine("\nDeveria? (S/N) ");
+bool deveria = Console.ReadLine()!.ToUpper().Trim().Substring(0, 1) == "S";
+Console.WriteLine();
+
+if (movendo)
+{
+    if (deveria)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Ótimo!");
+    }
+
+    else
+    {
+        Console.WriteLine("Use Silver Tape.");
+    }
+}
+
+else
+{
+    if (!deveria)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Ótimo!");
+    }
+
+    else
+    {
+        Console.WriteLine("Use WD-40.");
+    }
+}
+
+Console.ResetColor();
